b821058990ef1911a8283cb7f3578a5d4dfb4052
Marie Udam <marieudam@gmail.com>
2017-04-09 10:14:25 +0300

    ülesanne 1

--- praktikum8/yl1.php
+++ praktikum8/yl1.php
@@ -13,17 +13,17 @@ if (isset($_POST["background"]) && $_POST["background"]!="") {
     	$background=htmlspecialchars($_POST["background"]);
 	}
 
-$colortext="#000"; // vaikimisi värv
+$colortext="#000";
 	if (isset($_POST["colortext"]) && $_POST["colortext"]!="") {
     	$colortext=htmlspecialchars($_POST["colortext"]);
 	}
 
-$border="10"; // vaikimisi väärtus
+$border="10";
 	if (isset($_POST["border"]) && $_POST["border"]!="") {
     	$colortext=htmlspecialchars($_POST["border"]);
 	}
 
-$bordercolor="#000"; // vaikimisi väärtus
+$bordercolor="#000";
 	if (isset($_POST["bordercolor"]) && $_POST["bordercolor"]!="") {
     	$colortext=htmlspecialchars($_POST["bordercolor"]);
 	}
@@ -62,18 +62,9 @@ $bordercolor="#000"; // vaikimisi väärtus
 	<input type="color" name="background" value="#ff0000">&nbsp;Taustavärv<br><br>
 	<input type="color" name="colortext" value="#000000">&nbsp;Teksti värv<br><br>
 	<input type="number" name="border" min="1" max="20">&nbsp;Piirjoone laius (1-20px)<br><br>
-	<select name="cars">
-    	<option value="solid">Solid</option>
-    	<option value="dotted">Dotted</option>
-    	<option value="dashed">Dashed</option>
-    	<option value="Double">Double</option>
-  	</select>&nbsp;Piirjoone tüüp<br><br>
-  	<input type="color" name="bordercolor" value="#ffffff">&nbsp;Piirjoone värv<br><br>
-  	<input type="number" name="borderradius" min="1" max="100">&nbsp;Piirjoone nurga raadius (1-100px)<br><br>
+  	<input type="color" name="bordercolor" value="#000000">&nbsp;Piirjoone värv<br><br>
 	<input type="submit">
 </form>
-
-
 
 </body>
 
